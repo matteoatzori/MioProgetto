3f62578b01fbf77a773f8a85d8590c5e8d82f5e4
matteoatzori <rtf.atzorim@gmail.com>
2020-02-27 12:59:21 +0100

    initial commit2

---
+++ cmake-build-debug/CMakeFiles/MioProgetto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MioProgetto.dir/main.c.obj"
+  "MioProgetto.exe"
+  "MioProgetto.exe.manifest"
+  "MioProgetto.pdb"
+  "libMioProgetto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MioProgetto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
